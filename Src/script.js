8ccc8e2ba22c28cbb42f10e9398bddda8381d3c4
Avik Ghosh <avikghosh2605@gmail.com>
2023-07-29 12:03:46 +0530

    video and gif

--- Src/script.js
+++ Src/script.js
@@ -1,1 +1,20 @@
- 
+ let accordin = document.getElementsByClassName("faq-title")
+
+ for (let i =0; i < accordin.length; i++) {
+    accordin[i].addEventListener("click", function() {
+        if (this.childNodes[1].classList.contains("fa-plus")) {
+            this.childNodes[1].classList.remove("fa-plus");
+            this.childNodes[1].classList.add("fa-xmark");
+        } else {
+            this.childNodes[1].classList.remove("fa-plus");
+            this.childNodes[1].classList.add("fa-xmark"); 
+        }
+
+        let content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px"
+        }
+    });
+ }
